7e346b7453d35264fdc079ef7cd86f28c42c095c
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2021-08-25 17:56:14 +0530

    Added model for a video with required fields

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    id = models.CharField(max_length=100, unique=True, primary_key=True)
+    title = models.CharField(max_length=500)
+    description = models.TextField(max_length=1000)
+    thumbnails_url = models.URLField()
+    publishing_datetime = models.DateTimeField()
+
+    class Meta:
+        ordering = ('-publishing_datetime',)
+
+    def __str__(self):
+        return "{}, {}".format(self.id, self.title)

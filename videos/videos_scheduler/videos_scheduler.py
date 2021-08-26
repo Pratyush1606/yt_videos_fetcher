e73f4aff57406ebb6a68332083c1abb87a7ca4fa
Pratyush Jaiswal <48629423+Pratyush1606@users.noreply.github.com>
2021-08-26 21:05:32 +0530

    Update videos_scheduler.py

--- videos/videos_scheduler/videos_scheduler.py
+++ videos/videos_scheduler/videos_scheduler.py
@@ -3,5 +3,5 @@ from videos.views import fetch_videos_function
 
 def start():
 	scheduler = BackgroundScheduler()
-	scheduler.add_job(fetch_videos_function, "interval", seconds=10, id="weather_001",replace_existing=True)
+	scheduler.add_job(fetch_videos_function, "interval", seconds=10, id="videos_001",replace_existing=True)
 	scheduler.start()
